02751f325eef2eded14fea0c268eda67f518f2da
samar-sadaat <sammarsadaat@gmail.com>
2025-10-24 19:14:25 +0500

    created .env files

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/SchoolDB");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (err) {
         console.error(" MongoDB connection error:", err);
@@ -16,3 +18,10 @@ module.exports = connectDB;
 //             useNewUrlParser: true,
 //             useUnifiedTopology: true,
 //         }
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.log("MongoDB connection error:", err));
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const TeacherRoute = require('./src/routes/teacherRoutes');
 const StudentRoute = require('./src/routes/studentRoutes');
 const CourseRoutes = require('./src/routes/courseRoutes');
-const port = 4000;
+require('dotenv').config();
 const mongodb = require('./db')
 mongodb();
 
@@ -15,7 +15,8 @@ exp.use('/student', StudentRoute);
 exp.use('/course', CourseRoutes);
 
 
+const PORT = process.env.PORT || 4000;
 
-exp.listen(port, ()=>{
-    console.log(`Server is Runing ${port}`)
+exp.listen(PORT, ()=>{
+    console.log(`Server is Runing ${PORT}`)
 })
