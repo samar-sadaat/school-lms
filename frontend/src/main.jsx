90e9e9e8a0cc04aee52872cd75fa6ccff3c1ed7a
samar-sadaat <sammarsadaat@gmail.com>
2025-10-24 18:26:50 +0500

    added the package.json start

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
-    <>
+  <>
     <BrowserRouter>
       <App />
     </BrowserRouter>
